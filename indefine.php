126d1741e26706ea7b5fd2a8ababfe074cb43331
Payam Naderi <naderi.payam@gmail.com>
2014-11-08 11:52:11 +0330

    rename core to modules

--- indefine.php
+++ indefine.php
@@ -11,8 +11,8 @@ define('APP_DIR_ROOT'       , __DIR__ );
 define('APP_DIR_APPLICATION', APP_DIR_ROOT .DS. '_app');
 define('APP_DIR_LIBRARIES',   	APP_DIR_APPLICATION .DS. 'vendor');
 define('APP_DIR_MODULES', 		APP_DIR_APPLICATION .DS. 'vendor');
+define('APP_DIR_CORE', 			APP_DIR_APPLICATION .DS. 'modules');
 define('APP_DIR_CONFIG', 		APP_DIR_APPLICATION .DS. 'config');
-define('APP_DIR_CORE', 			APP_DIR_APPLICATION .DS. 'core');
 define('APP_DIR_TEMP', 			APP_DIR_APPLICATION .DS. 'tmp');
 define('APP_DIR_CACHE', 			APP_DIR_TEMP .DS. 'cache');
 
