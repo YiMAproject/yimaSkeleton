0298f78840953eccbcf2b25bed5a108a6f987249
Payam Naderi <naderi.payam@gmail.com>
2014-11-15 11:53:33 +0330

    error exception template

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
-// Define Consts and autoLoading
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(__DIR__);
+
+// Define Const and autoLoading
 require 'indefine.php';
 
 // startup preparation
@@ -9,13 +15,15 @@ if (file_exists(APP_DIR_CORE .DS. 'bootstrap.php')) {
 
 // Run the application!
 try {
-	// try to reach host specific config
+	// try to reach profile specific config
     $defaultConf = include  APP_DIR_CONFIG .DS. 'application.config.php';
 
     $domainConfFiles = APP_DIR_CONFIG .DS. 'profiles' .DS. APP_PROFILE .DS. 'application.override.{,local.}config.php';
     foreach (glob($domainConfFiles, GLOB_BRACE) as $file) {
         // merge with default config
+        ob_start();
         $hostConf = include $file;
+        ob_get_clean();
         $defaultConf = \Zend\Stdlib\ArrayUtils::merge($defaultConf, $hostConf);
     }
 
@@ -23,9 +31,7 @@ try {
 	Zend\Mvc\Application::init($defaultConf)->run();
 }
 catch (Exception $e) {
-	echo '<pre>';
-	echo '<h2>It`s seems be an error</h2>';
-	echo '<p style="color:red;font-weight:bold;">'.$e->getMessage().'</p>';
-
-	throw $e;
+    ob_start();
+    include 'error'.DS.'general.phtml';
+    ob_end_flush();
 }
