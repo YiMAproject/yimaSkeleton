cb875a8a519f4887071823a58880adb1b9f625cd
Payam Naderi <naderi.payam@gmail.com>
2014-11-24 13:03:03 +0330

    code cleanup

--- _app/config/profiles/bootstrap.php
+++ _app/config/profiles/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
+// set default timezone to avoid getting warning errors
+ini_get('date.timezone') ?:
+    date_default_timezone_set('UTC');
+
 // define functions
 function p_r($var) {
     echo "<pre>";
