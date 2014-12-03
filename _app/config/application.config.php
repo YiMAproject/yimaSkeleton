cc6e2cc973da08842e2e56b013f6c07a749ab2ad
Payam Naderi <naderi.payam@gmail.com>
2014-12-03 16:42:20 +0330

    Fix application config

--- _app/config/application.config.php
+++ _app/config/application.config.php
@@ -11,36 +11,21 @@
  *
  */
 return array(
-    # modules name that used in application must list here
-    'modules' => array(
-        'AssetManager',
-        'Application',
-    ),
+    'service_manager_config' => [
+        'invokables' => [
+            'SharedEventManager'  => 'Zend\EventManager\SharedEventManager',
 
-    /* X1: every key present here can fetch later */
-	'db' => array(
-		'prefix' => '',
-	),
-
-    # ----------------------------------------------------
-
-    /**
-     * This way you can register some service (factory, invokable, ..)
-     * to serviceManager.
-     *
-     * also change default behavior of program for ModuleManager, EventManager
-     * by registering new one instead serviceManager defaults.
-     *
-     * @see \Zend\Mvc\Service\ServiceManagerConfig
-     */
-    'service_manager' => array(
-        'invokables' => array(
             // Set Some Default Listeners
             'ExceptionMvcStrategyListener' => 'yimaBase\Mvc\View\Listener\ExceptionMvcStrategyListener',
             'SendExceptionListener'        => 'yimaBase\Mvc\Listener\SendExceptionListener',
-        ),
-        'factories'  => array (
+        ],
+        'factories'  => [
             // just for study case reason ...
+            'EventManager'  => 'Zend\Mvc\Service\EventManagerFactory',
+
+            'Request'       => 'Zend\Mvc\Service\RequestFactory',
+            'Response'      => 'Zend\Mvc\Service\ResponseFactory',
+            'Router'        => 'Zend\Mvc\Service\RouterFactory',
 
             // you can replace application startup or default ServiceManagerConfig with your own services
             'ModuleManager' => 'Zend\Mvc\Service\ModuleManagerFactory', // default
@@ -59,36 +44,87 @@ return array(
              *   for services that present to listener
              */
             'ServiceListener' => 'Zend\Mvc\Service\ServiceListenerFactory', // default
-        ),
-    ),
-    # ----------------------------------------------------
+        ],
+        'shared' => [
+            'EventManager' => false,
+        ],
+        'initializers' => [
+            'yimaBase\Mvc\Application\DefaultServiceInitializer'
+        ],
+    ],
 
     /*
      * Listeners config is a service that fetch from serviceManager
-     * and attach to EventManager in Application::init()
+     * and attach to EventManager in Application::initialize()
      */
-    'listeners' => array(
+    'listeners' => [
         'ExceptionMvcStrategyListener',
         'SendExceptionListener',
-    ),
+    ],
 
-    // ---------------------
-    // DON`T EDIT LINES BELOW
-    // if you want some changes override configs by domain spec file
-    // ===========================
+    'application_config' => [
+        # modules name that used in application must list here
+        'modules' => [
+            'AssetManager',
+            'Application',
+        ],
 
-    /**
-     * Used by default Module Manager,
-     *
-     * DefaultListenerAggregate will use it.
-     * DefaultListenerAggregate setup eventManager for LoadingModules
-     */
-    'module_listener_options' => array(
-		'module_paths' => array(
-			APP_DIR_CORE,
-			APP_DIR_MODULES,
-		),
-        'config_glob_paths'    => array(
+        // ---------------------
+        // DON`T EDIT LINES BELOW
+        // if you want some changes override configs by domain spec file
+        // ===========================
+
+        /**
+         * Used by default Module Manager,
+         *
+         * DefaultListenerAggregate will use it.
+         * DefaultListenerAggregate setup eventManager for LoadingModules
+         */
+        'module_listener_options' => [
+            'module_paths' => [
+                APP_DIR_CORE,
+                APP_DIR_MODULES,
+            ],
+            'config_glob_paths'    => [
+                # in file haa bar asaase tartib e gharaar giri include mishavand
+                APP_DIR_CONFIG .DS. 'modules.{,local.}config.php',
+                APP_DIR_CONFIG .DS. '{,*.}{global,local}.php',
+                APP_DIR_CONFIG .DS. '{,*.}{,*.}{global.,local.}config.php',
+                APP_DIR_CONFIG .DS. 'profiles' .DS. APP_PROFILE .DS. 'modules.override.{,local.}config.php',
+                APP_DIR_CONFIG .DS. 'profiles' .DS. APP_PROFILE .DS. '{,*.}{global,local}.php',
+                APP_DIR_CONFIG .DS. 'profiles' .DS. APP_PROFILE .DS. '{,*.}{,*.}{global.,local.}config.php',
+            ],
+            # caching modules merged config
+            'config_cache_enabled'  => false,
+            'cache_dir'    		   	=> APP_DIR_CACHE,
+            'config_cache_key' 	    => 'modulesMergedConfig_'.APP_PROFILE, // name of cache file
+
+            # also these options can beign set
+            /*
+            'config_static_paths'  => array(
+
+            ),
+            'extra_config'         => array(
+            ),
+            */
+        ],
+    ],
+
+    # ========================================================================================
+    # ==========================          DEPRECATED          ================================
+    # ========================================================================================
+
+    'modules' => [
+        'AssetManager',
+        'Application',
+    ],
+
+    'module_listener_options' => [
+        'module_paths' => [
+            APP_DIR_CORE,
+            APP_DIR_MODULES,
+        ],
+        'config_glob_paths'    => [
             # in file haa bar asaase tartib e gharaar giri include mishavand
             APP_DIR_CONFIG .DS. 'modules.{,local.}config.php',
             APP_DIR_CONFIG .DS. '{,*.}{global,local}.php',
@@ -96,19 +132,71 @@ return array(
             APP_DIR_CONFIG .DS. 'profiles' .DS. APP_PROFILE .DS. 'modules.override.{,local.}config.php',
             APP_DIR_CONFIG .DS. 'profiles' .DS. APP_PROFILE .DS. '{,*.}{global,local}.php',
             APP_DIR_CONFIG .DS. 'profiles' .DS. APP_PROFILE .DS. '{,*.}{,*.}{global.,local.}config.php',
+        ],
+        # caching modules merged config
+        'config_cache_enabled'  => false,
+        'cache_dir'    		   	=> APP_DIR_CACHE,
+        'config_cache_key' 	    => 'modulesMergedConfig_'.APP_PROFILE, // name of cache file
+
+        # also these options can beign set
+        /*
+        'config_static_paths'  => array(
+
         ),
-		# caching modules merged config
-		'config_cache_enabled'  => false,
-		'cache_dir'    		   	=> APP_DIR_CACHE,
-		'config_cache_key' 	    => 'modulesMergedConfig_'.APP_PROFILE, // name of cache file
-		
-		# also these options can beign set
-		/* 
-		'config_static_paths'  => array(
-			
-		),
-		'extra_config'         => array(
-		),
-		*/
-	),
+        'extra_config'         => array(
+        ),
+        */
+    ],
+
+    /**
+     * This way you can register some service (factory, invokable, ..)
+     * to serviceManager.
+     *
+     * also change default behavior of program for ModuleManager, EventManager
+     * by registering new one instead serviceManager defaults.
+     *
+     * @see \Zend\Mvc\Service\ServiceManagerConfig
+     */
+    'service_manager' => array(
+        'invokables' => array(
+            #'SharedEventManager'  => 'Zend\EventManager\SharedEventManager',
+
+            // Set Some Default Listeners
+            'ExceptionMvcStrategyListener' => 'yimaBase\Mvc\View\Listener\ExceptionMvcStrategyListener',
+            'SendExceptionListener'        => 'yimaBase\Mvc\Listener\SendExceptionListener',
+        ),
+        'factories'  => array (
+            // just for study case reason ...
+            #'EventManager'  => 'Zend\Mvc\Service\EventManagerFactory',
+
+            #'Request'       => 'Zend\Mvc\Service\RequestFactory',
+            #'Response'      => 'Zend\Mvc\Service\ResponseFactory',
+            #'Router'        => 'Zend\Mvc\Service\RouterFactory',
+
+            // you can replace application startup or default ServiceManagerConfig with your own services
+            'ModuleManager' => 'Zend\Mvc\Service\ModuleManagerFactory', // default
+
+            /* used by default Module Manager
+             *
+             * > by default containing all services for serviceManager and
+             *   using during running app. Controllers, view, and more more ...
+             *
+             * > serviceListener listen for Module.php and load some config file
+             *   by execute related method and get some config.
+             *   you can register some by "service_listener_options" key
+             *
+             * > attached to eventManager by default
+             *   setDefaultConfig catched from within modules
+             *   for services that present to listener
+             */
+            #'ServiceListener' => 'Zend\Mvc\Service\ServiceListenerFactory', // default
+        ),
+        'shared' => [
+            #'EventManager' => false,
+        ],
+        'initializers' => [
+            'yimaBase\Mvc\Application\DefaultServiceInitializer'
+        ],
+    ),
+    # ----------------------------------------------------
 );
