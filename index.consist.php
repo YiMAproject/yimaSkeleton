eee02b4ff938c19e8d179b5deb7252467f2943c4
Payam Naderi <naderi.payam@gmail.com>
2014-12-16 15:06:34 +0330

    add app general tmp folder

--- index.consist.php
+++ index.consist.php
@@ -16,9 +16,7 @@ define('APP_DIR_MODULES', 		APP_DIR_APPLICATION .DS. 'vendor');
 define('APP_DIR_CORE', 			APP_DIR_APPLICATION .DS. 'modules');
 define('APP_DIR_CONFIG', 		APP_DIR_APPLICATION .DS. 'config');
 define('APP_DIR_TEMP', 			APP_DIR_APPLICATION .DS. 'tmp');
-define('APP_DIR_CACHE', 			APP_DIR_TEMP .DS. 'cache');
 
 // Setup autoLoading
-if (file_exists(APP_DIR_LIBRARIES .DS. 'autoload.php')) {
+if (file_exists(APP_DIR_LIBRARIES .DS. 'autoload.php'))
     require_once APP_DIR_LIBRARIES .DS. 'autoload.php';
-}
